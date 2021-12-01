4bcc738dfbad3d1d413ca48518ae62b8e9b2241e
hazedrops <hazedrops@hotmail.com>
2021-11-30 22:24:45 -0800

    React - Modal Tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Modal from './Components/Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey, click on the button to open the modal.</h1>
+      <button 
+        className="openModalBtn" 
+        onClick={() => {
+          setOpenModal(true);
+        }}>Open</button>
+      {openModal && <Modal closeModal={setOpenModal} />}
     </div>
   );
 }
